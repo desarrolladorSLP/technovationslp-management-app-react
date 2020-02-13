9cc2ba1914a6545b88a9fbfe12ae70f452955e35
Sandra Marcela Herrera Arriaga <sandra.h.arriaga@gmail.com>
2020-02-13 14:57:11 -0600

    add content component base

---
+++ src/components/Content.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from '../assets/logo-10.png'
+
+
+function Content(){
+
+    return <div><img src={logo} alt="Logo Teckers" ></img></div>
+
+}
+export default Content
