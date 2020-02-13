15b7d842f98bb38e8255e1c4e357c8af16780f58
Alfredo <josalfk@hotmail.com>
2020-02-13 17:11:35 -0600

    implemented expandable card

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from '../button/Button'
 import {createUseStyles} from 'react-jss'
 import clsx from 'clsx'
 
 function Card (props){
-
+    const [expanded,setExpanded] = useState(false) 
     const useStyles = createUseStyles({
         Card: {
             width: 280,
             backgroundColor:'#FFFFFF',
             color: 'black',
             borderRadius: 4,
+            display: 'flex',
+            flexDirection: 'column',
             boxShadow:'0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12)'
           },
         
@@ -39,7 +41,8 @@ function Card (props){
             paddingLeft: 16,
             paddingRight: 16,
             height: 50,
-            textAlign: 'start'
+            textAlign: 'start',
+            marginTop:'auto'
         },
         'Actions > button':{
             marginRight: 8
@@ -61,7 +64,7 @@ function Card (props){
 
     const classes=useStyles()
 
-    return <div className = {classes.Card} >
+    return !expanded? <div className = {classes.Card} >
             <div className = {classes.Header}>
                 <div className = {classes.cardTitle}>{props.title}</div> 
                 
@@ -77,12 +80,20 @@ function Card (props){
                 
             </div>
             <div className={classes.Actions}>
-            <Button text ='Editar' color='#6d21b0' onlyText onClick={param => console.log('Hello My Friend')}/>
-                <Button text ='Eliminar' color='#6d21b0' onlyText onClick={param => console.log('Hello My Friend')}/>
+            <Button text ='Editar' color='#6d21b0' onlyText onClick={param => setExpanded(true)}/>
+                <Button text ='Eliminar' color='#6d21b0' onlyText onClick={() => console.log('Hello My Friend')}/>
             </div>
 
             
+            </div> :
+            <div className = {classes.Card} >
+                    {props.formFields}  
+                    <div className = {classes.Actions}>
+                        <Button text = "Save" color ="#6d21b0" onlyText ></Button>
+                        <Button text = "Cancel" color ="#6d21b0" onlyText onClick= {() => setExpanded(false) } ></Button>
+                    </div>
             </div>
+            
 
 }
 
