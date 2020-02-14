7489d891435e1548b243a0a9c0d23e22d19e9e16
Sandra Marcela Herrera Arriaga <sandra.h.arriaga@gmail.com>
2020-02-14 11:58:33 -0600

    modify .gitignore

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const config = {
+  apiKey: 'AIzaSyB9F7DKchhPZhO-hGw1sp6zpX-Q9HAOUZ8',
+  authDomain: 'teckers-app.firebaseapp.com',
+  projectId: 'teckers-app',
+  storageBucket: 'teckers-app.appspot.com',
+  firebase_url: 'https://teckers-app.firebaseio.com'
+}
