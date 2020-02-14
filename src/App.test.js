4cb5c335de34561e26bfd88a310e71e1f38c0f7e
Sandra Marcela Herrera Arriaga <sandra.h.arriaga@gmail.com>
2020-02-14 12:26:32 -0600

    fix app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
+/* global test */
 import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
 
-render.test('render(<App />)', () => {
+test('render(<App />)', () => {
   const { getByText } = render(<App />)
-  return getByText
 })
