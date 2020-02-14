abfa70cf989f45d99090804d6588cd1fddce00a1
Sandra Marcela Herrera Arriaga <sandra.h.arriaga@gmail.com>
2020-02-14 12:17:43 -0600

    run yarn lint-check

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-/* global test, expect */
 import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
 
-test('render(<App />)', () => {
+render.test('render(<App />)', () => {
   const { getByText } = render(<App />)
+  return getByText
 })
