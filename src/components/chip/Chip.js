57e6bf4297bfab5d802ece7c900aa86b11f4fb52
Alfredo <josalfk@hotmail.com>
2020-02-15 14:13:42 -0600

    added chip component

---
+++ src/components/chip/Chip.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { createUseStyles } from 'react-jss'
+
+function Chip (props) {
+  const useStyles = createUseStyles({
+    chip: {
+      height: 20,
+      borderRadius: 16,
+      paddingLeft: 12,
+      paddingRight: 8,
+      paddingTop: 6,
+      paddingBottom: 6,
+      margin: 4,
+      display: 'inline-flex',
+      backgroundColor: '#0000001f'
+    },
+    closeButton: {
+      marginLeft: 8,
+      color: '#969696',
+      fontSize: 20,
+      '&:hover': {
+        color: '#808080'
+      },
+      cursor: 'pointer'
+    }
+  })
+  const classes = useStyles()
+
+  function clickHandler () {
+    props.onClick()
+  }
+  return (
+    <div className={classes.chip}>
+      {props.text}
+      <div
+        className={classes.closeButton + ' material-icons'}
+        onClick={props.onClick ? clickHandler : null}
+      >
+        cancel
+      </div>
+    </div>
+  )
+}
+
+export default Chip
