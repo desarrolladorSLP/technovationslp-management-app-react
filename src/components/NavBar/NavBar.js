4dd1c2e11ac511cefd763b1f28da481930e88644
Sandra Marcela Herrera Arriaga <sandra.h.arriaga@gmail.com>
2020-02-17 11:11:44 -0600

    work with NavBar style

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,15 +7,17 @@ import Button from '../button/Button'
 
 function NavBar(){
 
-    return <nav><img src={logo} alt="Logo Teckers" ></img>
+    return <nav>
     <div className="logo-navbar"> 
-    <Button text= "Prueba" color="#000000" onClick={  param => console.log("Se presiono el boton")} ></Button>
-    <Button text= "Programas" color="#000000" onClick={  param => console.log("Se presiono el boton")} ></Button> 
-    <Button text= "Usuarios" color="#000000" onClick={  param => console.log("Se presiono el boton")} ></Button> 
-    <Button text= "Otro" color="#000000" onClick={  param => console.log("Se presiono el boton")} ></Button>
-    <ButtonFirebase></ButtonFirebase>  </div>
-    
-    
+    <ul>
+    <li><img src={logo} alt="Logo Teckers" ></img></li>
+    <li><Button text= "Prueba" color="#000000" onClick={  param => console.log("Se presiono el boton")} ></Button></li>
+    <li><Button text= "Programas" color="#000000" onClick={  param => console.log("Se presiono el boton")} ></Button> </li>
+    <li><Button text= "Usuarios" color="#000000" onClick={  param => console.log("Se presiono el boton")} ></Button></li> 
+    <li><Button text= "Otro" color="#000000" onClick={  param => console.log("Se presiono el boton")} ></Button></li>
+    <li><ButtonFirebase></ButtonFirebase></li>
+    </ul>
+    </div>
     </nav>
 }
 export default NavBar
